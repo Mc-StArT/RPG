09f4ae2d7d9ffcab391f51ae7cfa44fa83e3cee4
ADmi <alexander.starkin@gmail.com>
2021-04-11 19:54:26 +0300

    added person+animation

---
+++ Assets/scripts/move.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+    public float speed;
+    private float hor;
+    private float ver;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hor = Input.GetAxis("Horizontal") * speed;
+        ver = Input.GetAxis("Vertical") * speed;
+        transform.Translate(new Vector3(hor, 0, ver));
+    }
+}
